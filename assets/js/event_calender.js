a0b576aeeeae70f02a9684086898f629df54cfe2
Waqar-107 <waqar.hassan866@gmail.com>
2020-02-21 13:17:00 +0600

    calender fixed, added a part of the tentative schedule

--- assets/js/event_calender.js
+++ assets/js/event_calender.js
@@ -7,10 +7,94 @@ event_in_fest = [
 
     description: 'Hackathon',
     event_time: '12 March - 13 March',
-    location: 'BUET Gym',
+    location: 'to be decided',
 
-    imageName: '<i class="lnr lnr-laptop"></i>',
+    imageName: '<i class="lnr lnr-laptop calender_logo"></i>',
     link: '"events/hackathon.html"'
+  },
+
+  {
+    start_date: '2020/3/19',
+
+    date_to_show: '19',
+    month_to_show: ' March',
+
+    description: 'Treasure Hunt',
+    event_time: '19 March',
+    location: 'to be decided',
+
+    imageName: '<i class="calender_logo"><span class="iconify" data-icon="mdi:treasure-chest" data-inline="false"></span></i>',
+    link: '"events/treasure_hunt.html"'
+  },
+
+  {
+    start_date: '2020/3/20',
+
+    date_to_show: '20',
+    month_to_show: ' March',
+
+    description: 'Game Fest',
+    event_time: '20 March - 21 March',
+    location: 'to be decided',
+
+    imageName: '<i class="calender_logo"><span class="iconify" data-icon="entypo:game-controller" data-inline="false"></span></i>',
+    link: '"events/game_fest.html"'
+  },
+
+  {
+    start_date: '2020/3/21',
+
+    date_to_show: '21',
+    month_to_show: ' March',
+
+    description: 'Math Olympiad',
+    event_time: '21 March',
+    location: 'to be decided',
+
+    imageName: '<i class="icon-linegraph calender_logo"></i>',
+    link: '"events/math_olympiad.html"'
+  },
+
+  {
+    start_date: '2020/3/27',
+
+    date_to_show: '27',
+    month_to_show: ' March',
+
+    description: 'IUPC',
+    event_time: '27 March',
+    location: 'to be decided',
+
+    imageName: '<i class="lnr lnr-keyboard calender_logo"></i>',
+    link: '"events/iupc.html"'
+  },
+
+  {
+    start_date: '2020/4/1',
+
+    date_to_show: '1',
+    month_to_show: 'April',
+
+    description: 'Job Fair',
+    event_time: '1 April',
+    location: 'to be decided',
+
+    imageName: '<i class="ion-ios-people-outline calender_logo"></i>',
+    link: '"events/job_fair.html"'
+  },
+
+  {
+    start_date: '2020/4/2',
+
+    date_to_show: '2',
+    month_to_show: 'April',
+
+    description: 'Grand Cultural',
+    event_time: '2 April',
+    location: 'to be decided',
+
+    imageName: '<i class="lnr lnr-mic calender_logo"></i>',
+    link: '"events/grand_cultural.html"'
   },
 ];
 
@@ -33,13 +117,8 @@ for (let i = 0; i < event_in_fest.length; i++) {
       "<h6>" + event_in_fest[i].event_time + "</h6><p>" + event_in_fest[i].location + "</p></div></td>" +
 
       "<td>" + "<a href=" + event_in_fest[i].link + " class=\"btn btn-primary btn-rounded\">Read More</a>" +
-      "</td></tr>";
+      "</td>" + "</tr>";
 
     $(".eventCalender").append(htm)
   }
 }
-<div class="event_place">
-  <h5>Conference in Amsterdam</h5>
-  <h6>08 AM - 04 PM</h6>
-  <p>Speaker: Daniel Hill</p>
-</div>
