8d460156988afe46bfcd91aa0465d23a25f26a5f
Waqar-107 <waqar.hassan866@gmail.com>
2020-02-29 00:53:16 +0600

    color changed according to theme

--- assets/js/main.js
+++ assets/js/main.js
@@ -60,7 +60,7 @@ $(document).ready(function () {
     });
 
     $(".countdown")
-        .countdown("2020/03/10", function (event) {
+        .countdown("2020/03/18", function (event) {
             $(this).html(
                 event.strftime('<div>%w <span>Weeks</span></div>  <div>%d <span>Days</span></div>  <div>%H<span>Hours</span></div> <div>%M<span>Minutes</span></div> <div>%S<span>Seconds</span></div>')
             );
